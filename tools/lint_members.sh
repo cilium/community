a4e65360df7471df53163386b3f3b057e7315a29
Jef Spaleta <jspaleta@gmail.com>
2025-02-14 16:38:32 -0900

    tools: test member usernames against GH API
    GitHub API is case-sensitive for submitted usernames. Refactor the linter
    to use the GitHub REST API to check that the specified usernames are
    valid for subsequent GitHub API calls.
    
    Make org check conditional on GITHUB_TOKEN permissions.
    
    Signed-off-by: Jef Spaleta <jspaleta@gmail.com>

--- tools/lint_members.sh
+++ tools/lint_members.sh
@@ -29,70 +29,129 @@ url="https://api.github.com/orgs/$2/members"
 page=1
 flag=true
 logins=()
+check_org_members=true
 while $flag; do
   status=$(curl --head --location --connect-timeout 5 --write-out %{http_code} --silent --output /dev/null -H "Authorization: Bearer $GITHUB_TOKEN" ${url}?page=${page})
   if [[ "$status" -gt 399 ]] ; then
-    echo "GitHub API Status Code: $status"
-    exit 1
-  fi
-  response=$( curl -s -I -H "Authorization: Bearer $GITHUB_TOKEN" "${url}?page=${page}" )
-  # Append to logins array
-  logins+=( `curl --no-progress-meter -L \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $GITHUB_TOKEN" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    ${url}?page=${page} | jq -r '.[].login'` )
-
-  # Check to see if there is a next page to grab
-  set +o pipefail
-  next_page=$(echo "$response" | grep -s 'rel="next"' | sed 's/.*<//' | sed 's/>.*//')
-  set -o pipefail
-
-  if [[ -z "$next_page" ]]; then
+    echo "GitHub API Org Membership Retrieval Failure with Status Code: $status"
     flag=false
+    check_org_members=false
   fi
-  
-  page=$((page + 1))
-done
+  if $flag; then
+    response=$( curl -s -I -H "Authorization: Bearer $GITHUB_TOKEN" "${url}?page=${page}" )
+    # Append to logins array
+    logins+=( `curl --no-progress-meter -L \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer $GITHUB_TOKEN" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      ${url}?page=${page} | jq -r '.[].login'` )
 
->&2 echo "Org Members:"
-for login in "${logins[@]}"; do
-  >&2 echo "$login"
+    # Check to see if there is a next page to grab
+    set +o pipefail
+    next_page=$(echo "$response" | grep -s 'rel="next"' | sed 's/.*<//' | sed 's/>.*//')
+    set -o pipefail
+
+    if [[ -z "$next_page" ]]; then
+      flag=false
+    fi
+  
+    page=$((page + 1))
+  fi
 done
+if $check_org_members; then
+  >&2 echo "Org Members:"
+  for login in "${logins[@]}"; do
+    >&2 echo "$login"
+  done
+fi
 >&2 echo ""
 
 status=0
 for file in *.yaml; do
-     >&2 echo -e "\n>> Linting members/mentors from $file"
+     >&2 echo -e "\n>> Linting members from $file"
      f="${file%.*}"
      cd "$CDIR"; cd "$1"
-     members=`yq -o t '.members' $file`
-     for member in $members; do
-       #>&2 echo "checking team member: $member"
-       found=0
-       for login in "${logins[@]}"; do
-         if [ "$login" == "$member" ] ; then
-           found=1
-         fi
-       done
-       if [ $found -eq 0 ] ; then
-         >&2 echo "Team $f Member: $member not found in Org: $2" 
-         status=1
-       fi
-     done
+     set +e 
+     members=$(yq -e -o t '.members' $file 2>/dev/null)
+     ret=$?
+     set -e
+     if [ $ret -ne 0 ]; then
+       continue
+     fi
+     for member in ${members[@]}; do
+       if [ -n "$member" ]; then
 
-     mentors=`yq -o t '.mentors' $file`
-     for mentor in $mentors; do
-       #>&2 echo "checking team mentor: $mentor"
-       found=0
-       for login in "${logins[@]}"; do
-         if [ "$login" == "$mentor" ] ; then
-           found=1
+         #>&2 echo "checking team member: $member"
+         # Checking to see if team member login is cased correctly
+         http_status=`curl --header "Authorization: Bearer $GITHUB_TOKEN" --head --location --connect-timeout 5 --write-out %{http_code} --silent --output /dev/null --no-progress-meter "https://api.github.com/users/${member}"`
+         if [[ "$http_status" -gt 399 ]] ; then
+	   >&2 echo "Error: Member: ${member} :: GitHub API Status Code: $http_status"
+           status=1
+         else
+           login=`curl --header "Authorization: Bearer $GITHUB_TOKEN" --no-progress-meter "https://api.github.com/users/${member}" | jq -r .login`
+           if [ "$login" == "$member" ] ; then
+             found=1
+           else
+             >&2 echo "Team $f Member: $member case mistatch should be : $login" 
+             status=1
+           fi
          fi
-       done
-       if [ $found -eq 0 ] ; then
-         >&2 echo "Team $f Mentor: $mentor not found in Org: $2" 
-         status=1
+
+         # Checking to see if team member login is in org membership
+         if $check_org_members; then
+           found=0
+           for login in "${logins[@]}"; do
+             if [ "$login" == "$member" ] ; then
+               found=1
+             fi
+           done
+           if [ $found -eq 0 ] ; then
+             >&2 echo "Team $f Member: $member not found in Org: $2" 
+             status=1
+           fi
+         fi
+       fi  
+     done
+     >&2 echo -e "\n>> Linting mentors from $file"
+
+     set +e
+     mentors=$(yq -e -o t '.mentors' $file 2>/dev/null)
+     ret=$?
+     set -e
+     if [ $ret -ne 0 ]; then
+       continue
+     fi
+     for mentor in ${mentors[@]}; do
+       if [ -n "$mentor" ]; then
+         >&2 echo "checking team mentor: $mentor"
+         # Checking to see if team mentor login is cased correctly
+         http_status=`curl --header "Authorization: Bearer $GITHUB_TOKEN" --head --location --connect-timeout 5 --write-out %{http_code} --silent --output /dev/null --no-progress-meter "https://api.github.com/users/${mentor}"`
+         if [[ "$http_status" -gt 399 ]] ; then
+	   >&2 echo "Error: Mentor: ${mentor} :: GitHub API Status Code: $http_status"
+           status=1
+         else
+           login=`curl --header "Authorization: Bearer $GITHUB_TOKEN" --no-progress-meter "https://api.github.com/users/${mentor}" | jq -r .login`
+           if [ "$login" == "$mentor" ] ; then
+             found=1
+           else
+             >&2 echo "Team $f Mentor: $mentor case mistatch should be : $login" 
+             status=1
+           fi
+         fi
+
+         # Checking to see if team mentor login is in org membership
+         if $check_org_members; then
+           found=0
+           for login in "${logins[@]}"; do
+             if [ "$login" == "$mentor" ] ; then
+               found=1
+             fi
+           done
+           if [ $found -eq 0 ] ; then
+             >&2 echo "Team $f Mentor: $mentor not found in Org: $2" 
+             status=1
+           fi
+         fi
        fi
      done
 done
