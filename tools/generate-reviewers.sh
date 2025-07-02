bb7a01a15d521150f40b51eb376d0a32b9ed37bf
Joe Stringer <joe@cilium.io>
2025-07-02 16:03:24 -0700

    tools: Add generate-reviewers.sh script
    Add a script to generate the reviewers from the list of code owner teams
    under 'ladder/teams'. Keep the resulting reviewer team outside, directly
    under 'ladder/' to ensure it is subject to committer review prior to
    merge.
    
    Signed-off-by: Joe Stringer <joe@cilium.io>

---
+++ tools/generate-reviewers.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Copyright Authors of Cilium
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+REVIEWERS="ladder/reviewers.yaml"
+
+function main() {
+    echo "reviewers:" > $REVIEWERS
+    for f in ladder/teams/*; do
+        yq '.members' "$f";
+    done >> $REVIEWERS.new
+    LANG=C sort -u $REVIEWERS.new >> $REVIEWERS
+    rm $REVIEWERS.new
+}
+
+main "$@"
